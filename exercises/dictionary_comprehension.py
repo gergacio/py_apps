85b601e3427476c3003fb870fd97abdca65d718d
Georgi <ggeorgeuk@gmail.com>
2024-01-19 23:34:06 +0000

    init

---
+++ exercises/dictionary_comprehension.py
@@ -0,0 +1,14 @@
+sentence = input()
+
+# Using dictionary comprehension
+result = {word:len(word) for word in sentence.split()}
+
+print(result)
+
+# eval() converts correctly formatted string to dict
+# weather_c = eval(input())
+
+# dictionary comprehension
+# weather_f = {day:temp * 9/5 + 32 for (day, temp) in weather_c.items()}
+
+# print(weather_f)
