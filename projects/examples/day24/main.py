13d1cc4e210dd476c8bcb4e33969b4c6d1875cb6
Georgi <ggeorgeuk@gmail.com>
2024-01-27 20:50:49 +0000

    update

---
+++ projects/examples/day24/main.py
@@ -0,0 +1,6 @@
+# with open("my_file.txt") as file:
+#     content = file.read()
+#     print(content)
+
+with open("new_file.txt", mode='a') as file:
+    file.write("\nNew file")
--- projects/snake/main.py
+++ projects/snake/main.py
@@ -9,7 +9,6 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
-
 snake = Snake()
 food = Food()
 scoreboard = Scoreboard()
