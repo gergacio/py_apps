55649d7c037e8ca2b775511c1d6a95621b5e373e
Georgi <ggeorgeuk@gmail.com>
2024-02-05 22:14:37 +0000

    init

--- projects/weather_data_app/main.py
+++ projects/weather_data_app/main.py
@@ -13,3 +13,18 @@ import pandas
 data = pandas.read_csv("weather_data1.csv")
 print(data["temp"])
 
+data_dict = data.to_dict()
+print(data_dict)
+
+data_list = data["temp"].to_list()
+print(data_list)
+
+# avarage = sum(data_list) / len(data_list)
+# print(avarage)
+
+print(data["temp"].mean())
+print(data["temp"].max())
+print(data[data.day == "Monday"])
+print(data[data.temp == data.temp.max()])
+
+
