025b7aa20a535d4225a99d5e98271a09e9131369
Georgi <ggeorgeuk@gmail.com>
2024-02-12 19:24:04 +0000

    update

--- projects/us_states/main.py
+++ projects/us_states/main.py
@@ -15,16 +15,26 @@ turtle.shape(image)
 
 data = pandas.read_csv("50_states.csv")
 all_states = data.state.to_list()
+guessed_state = []
 
-answer_state = screen.textinput(title = "Guess the State.", prompt="What's another state's name.")
-print(answer_state)
+while len(guessed_state) < 50:
+    answer_state = screen.textinput(title = f"{len(guessed_state)}/50 States Correct",
+                                    prompt="What's another state's name.").title()
+    print(answer_state)
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_state:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
 
-if answer_state in all_states:
-    t = turtle.Turtle()
-    t.hideturtle()
-    t.penup()
-    state_data = data[data.state == answer_state]
-    t.goto(int(state_data.x), int(state_data.y))
-    t.write(answer_state)
-
-screen.exitonclick()
+    if answer_state in all_states:
+        guessed_state.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
